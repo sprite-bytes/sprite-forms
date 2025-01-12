4005ccb565d7dcd6844003135971c82462543b02
格子 <fl9420@qq.com>
2025-01-12 13:53:18 +0800

    feat(SpriteTable): 表格使用

--- examples/table-usage/index.vue
+++ examples/table-usage/index.vue
@@ -1,30 +1,76 @@
 <script setup lang="ts">
-import {ref} from "vue";
+import {reactive} from "vue";
+import SpriteTable from "@packages/components/sprite-table/sprite-table.vue";
+import type {ColumnItem, TableConfig} from "@packages/types";
+import {FormElemType} from "@packages/enums";
 
-const columns = ref([
+const getOptions = (params: any) => {
+  const list = [{value: 1, name: `党员${params.label}`,}, {value: 2, name: `团员${params.label}`}]
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve(list)
+    }, 2000)
+  })
+}
+
+const columns = reactive<ColumnItem[]>([
   {
     name: 'name',
     label: '姓名',
+    component: FormElemType.INPUT
   },
   {
     name: 'age',
-    label: '年龄'
+    label: '年龄',
+    component: FormElemType.INPUT_NUMBER
   },
   {
     name: 'gender',
     label: '性别',
-    format: () => {
-      return '男'
+    component: ({row}: any) => {
+      return row.age < 10 ? FormElemType.SELECT : FormElemType.RADIO
+    },
+    options: [{value: 1, label: '男'}, {value: 2, label: '女'}],
+    change: async (params) => {
+      const {getPropsByField, loadOptions} = params
+      const props = getPropsByField('name')
+      if (props) {
+        props.label = '学生姓名'
+      }
+      const result = await loadOptions({
+        targetField: 'politicalOutlook',
+        params: {
+          label: '%'
+        },
+      })
+      console.log('result=>', result)
+      console.log('props=>', props)
+      console.log(params)
     }
-  }
-])
-const data = ref([
-  {name: '格子', age: 18, gender: '1'},
-  {name: '格子大暑版本', age: 20, gender: '0'}
+  },
+  {
+    component: FormElemType.SELECT,
+    label: '政治面貌',
+    name: 'politicalOutlook',
+    labelKey: 'name',
+    valueKey: 'value',
+    required: true,
+    remoteOptions: ({params}: any) => {
+      return getOptions({label: params?.label || '#'})
+    }
+  },
 ])
 
-const config = ref({
-  border: true,
+const data = reactive([
+  {name: '格子', age: 18, gender: 1},
+  {name: '格子大暑版', age: 20, gender: 2},
+  {name: '格子惊蛰版', age: 20, gender: 1}
+])
+
+const config = reactive<TableConfig>({
+  tableProps: {
+    border: true,
+  }
 })
 </script>
 
